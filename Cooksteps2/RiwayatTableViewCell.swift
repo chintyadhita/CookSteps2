e862e8c7e04301eb5f24ebad935e537260ff9099
chintyadhita <chintyadhitaaa@gmail.com>
2022-04-07 10:42:27 +0700

    history page without action button

---
+++ Cooksteps2/RiwayatTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RiwayatTableViewCell.swift
+//  Cooksteps2
+//
+//  Created by Cyntya Rahma Dita on 06/04/22.
+//
+
+import UIKit
+
+class RiwayatTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func didTapButton() {
+        
+    }
+
+}
